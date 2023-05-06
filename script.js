a806f3fb17f8b9bf528f813c05956f7e489b25f5
Hendardinata <muhammadhifzil19@gmail.com>
2023-05-06 15:29:31 +0800

    Upload seluruh file Program | HTML CSS JS

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch("https://6450dbfba32219691153974a.mockapi.io/api/v1/signature")
+  .then((Response) => Response.json())
+  .then((Response) => {
+    const images = document.getElementById("card-signature");
+    Response.forEach((signature) => {
+      images.innerHTML += `<div class="col-3">
+        <div class="card">
+          <img src="${signature.gambar}" class="card-img-top" alt=".....">
+            <div class="card-body">
+            <h5 class="card-title">${signature.judul}</h5>
+            <p class="card-text">${signature.deskripsi}</p>
+            </div>
+        </div>
+      </div>`;
+    });
+  });
+
+fetch("https://6450dbfba32219691153974a.mockapi.io/api/v1/program")
+  .then((Response) => Response.json())
+  .then((Response) => {
+    const images = document.getElementById("card-porgram");
+    Response.forEach((program) => {
+      images.innerHTML += `<div class="swiper-slide">
+        <div class="card">
+          <img src="${program.image}" class="card-img-top" alt=".....">
+            <div class="card-body">
+            <h5 class="card-title">${program.judul}</h5>
+            <p class="card-text">${program.deskripsi}</p>
+            </div>
+        </div>
+      </div>`;
+    });
+  });
